fec2af1c55922498644ca159a12f8c1d16316f24
aaronmcgill <aaron.garcia@mail.mcgill.ca>
2023-05-21 19:39:03 -0400

    added successful DB connection

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,26 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+import Image from "next/image";
+import { createClient } from "@vercel/postgres";
 
-export default function Home() {
+import styles from "./page.module.css";
+
+export default async function Home() {
+  const client = createClient();
+  await client.connect();
+
+  const { rows } = await client.sql`
+SELECT name, message
+FROM messages`;
+
+  console.log(rows);
+
   return (
     <main className={styles.main}>
-      <p>Hello NExt!</p>
+      {rows.map((r) => (
+        <span>
+          <span>{r.name}</span>
+          <span>{r.message}</span>
+        </span>
+      ))}
     </main>
-  )
+  );
 }
