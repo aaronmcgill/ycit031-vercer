c0787d6d83736dc04a41c7bc737056b57a106c3b
aaronmcgill <aaron.garcia@mail.mcgill.ca>
2023-05-29 21:14:27 -0400

    re-organized files

--- src/app/page.js
+++ src/app/page.js
@@ -1,15 +1,15 @@
 import Image from "next/image";
 import { createClient } from "@vercel/postgres";
 
-import styles from "./page.module.css";
+import styles from "/css/page.module.css";
 
 export default async function Home() {
   const client = createClient();
   await client.connect();
 
   const { rows } = await client.sql`
-SELECT name, message
-FROM messages`;
+    SELECT name, message
+    FROM messages`;
 
   console.log(rows);
 
